f0f5a2f8c3e8da0563c0ecdfadf689aea0c6062c
Roman Mikhaylov <audioveda@gmail.com>
2019-05-22 22:19:21 +0300

    1.3.5 + 1.4.1

--- Lesson1/90triangle.rb
+++ Lesson1/90triangle.rb
@@ -6,12 +6,10 @@ a, b, c = [a, b, c].sort!
 result = "банальный, совершенно никакой"
 if a == b && b == c 
   result = "равносторонний"
-else 
-  if a**2 + b**2 == c**2 
+elsif a**2 + b**2 == c**2 
     result = "прямоугольный"
-    if a == b || b == c || c == a  
+    if a == b  
       result = result + "да еще и равнобедренный"
     end
-  end
 end
 puts "Сегодня ты у нас #{result} треугольник"
---
+++ Lesson1/equation.rb
@@ -0,0 +1,22 @@
+#help_url = "http://www.bolshoyvopros.ru/questions/299829-kak-sostavit-algoritm-reshenija-kvadratnogo-uravnenija.html"
+
+puts "Введите последовательно коэффициенты уравнения."
+a = gets.to_i
+b = gets.to_i
+c = gets.to_i
+
+puts "Итак, вот наше уравнение #{a}x² + #{b}x + #{c} = 0"
+d = b**2 - 4*a*c
+puts "Дискриминант D, который считаетсчя по формуле «D = (b² – 4ac)» равен у нас #{d}"
+
+if d > 0
+  d_sqrt = Math.sqrt(d)
+  x1 = (-b + d_sqrt) / (2.0 * a)
+  x2 = (-b - d_sqrt) / (2.0 * a)
+  puts "Корни уравнения #{x1} и #{x2}"
+elsif d == 0
+ x = (-b) / (2.0 * a)
+  puts "корень уравнения #{x}"
+else
+  puts "поэтому корней не ждите, их нет"
+end
