ad262e6ee7e815f5888e0d219c733ddc7bd2e50d
Roman Mikhaylov <audioveda@gmail.com>
2019-07-11 15:51:33 +0700

    Lesson4

--- Lesson4/wagons/passenger_wagon.rb
+++ Lesson4/wagons/passenger_wagon.rb
@@ -1,5 +1,6 @@
 require_relative 'wagon'
 
+# создаем дочерний класс пассажирского вагона на основе класса вагон, разница лишь в том, что вызывая инстанцирование класса PassengerWagon мы сразу же получаем продвинутый вагон с типом "пассажирский"
 class PassengerWagon < Wagon
   def initialize
     @type = "Passenger"
